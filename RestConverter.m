9eb36c4a8825db0b0443fd0a9cf0ffe3ac6d3c22
Idan Gazit <idan@pixane.net>
2009-02-24 01:07:04 +0200

    Basic Safety: Py_DECREF to free content, NULL checking.

--- RestConverter.m
+++ RestConverter.m
@@ -2,10 +2,14 @@
 #import <Python/Python.h>
 
 NSData* renderRest(NSURL* url) {
+	// Renders the contents of a NSURL from rest source into HTML.
+	// Requires that Python and docutils be installed.
+	// Returns NULL for any kind of failure.
 	
-	PyObject *pmodule, *pmethod, *presult;
 	PyTupleObject *pmethodargs;
 	PyDictObject *pmethodnamedargs;
+	PyObject *psource, *pwriter, *pmodule, *pmethod, *presult;
+	
 	char* output;
 	NSString *source = [NSString stringWithContentsOfFile:[url path] encoding:NSUTF8StringEncoding error:nil];
 	
@@ -15,21 +19,49 @@ NSData* renderRest(NSURL* url) {
 	// TODO: Is it possible to make the warnings go away?
 	
 	pmethodargs = PyTuple_New(1);
+	if (pmethodargs == NULL) return NULL;
 	pmethodnamedargs = PyDict_New();
+	if (pmethodnamedargs == NULL) return NULL;
 	
-	PyTuple_SetItem(pmethodargs, 0, PyString_FromString([source UTF8String]));
-	PyDict_SetItemString(pmethodnamedargs, "writer_name", PyString_FromString("html"));
+	psource = PyString_FromString([source UTF8String]);
+	if (psource == NULL) return NULL;
+	PyTuple_SetItem(pmethodargs, 0, psource);
+	
+	pwriter = PyString_FromString("html");
+	if (pwriter == NULL) return NULL;
+	PyDict_SetItemString(pmethodnamedargs, "writer_name", pwriter);
 	pmodule = PyImport_ImportModule("docutils.core");
-	pmethod = PyObject_GetAttrString(pmodule, "publish_string");
-	presult = PyObject_Call(pmethod, pmethodargs, pmethodnamedargs);	
-	if (PyErr_Occurred())
-		PyErr_Print();
-	PyArg_Parse(presult, "s", &output);
-	if (PyErr_Occurred())
-		PyErr_Print();
+	//if (PyErr_Occurred())
+	//	PyErr_Print();
+	if (pmodule == NULL) return NULL;
 	
-	Py_Finalize();
-		
+	pmethod = PyObject_GetAttrString(pmodule, "publish_string");
+	//if (PyErr_Occurred())
+	//	PyErr_Print();
+	if (pmethod == NULL) return NULL;
+	
+	presult = PyObject_Call(pmethod, pmethodargs, pmethodnamedargs);	
+	//if (PyErr_Occurred())
+	//	PyErr_Print();
+	if (presult == NULL) return NULL;
+	
+	PyArg_Parse(presult, "s", &output);
+	//if (PyErr_Occurred())
+	//	PyErr_Print();
+	
+	
+	Py_DECREF(presult);
+	Py_DECREF(pmethod);
+	Py_DECREF(pmodule);
+	Py_DECREF(pwriter);
+	Py_DECREF(psource);
+	Py_DECREF(pmethodnamedargs);
+	Py_DECREF(pmethodargs);
+	
 	NSString *html = [NSString stringWithUTF8String:output];
+	
+	// TODO: Why does this cause segfault?
+	//Py_Finalize();
+		
 	return [html dataUsingEncoding:NSUTF8StringEncoding];
 }
